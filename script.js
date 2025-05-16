64b3c316d5d1fd4a58bac82cf3494b91b5f24245
sargunad <sargunad17@gmail>
2025-05-16 15:57:43 +0530

    first comment

---
+++ script.js
@@ -0,0 +1,21 @@
+var a=10;
+var b=20;
+console.log(a+b)
+console.log(a*b)
+console.log(a>b)
+console.log(a<b)
+console.log(a==b)
+console.log(a/b)
+console.log(a&b)
+function add()
+{
+    var a=10;
+    var b=20;
+    console.log(a+b)
+}
+add()
+function add(a,b)
+{
+    console.log(a+b)
+}
+add(10,40)
