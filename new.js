9d88304affddf84e8e61debee2e7860cef79f02b
sargunad <sargunad17@gmail>
2025-05-19 15:35:24 +0530

    pushed file

--- new.js
+++ new.js
@@ -23,3 +23,51 @@ for(n=1;n<=i;n++)
     console.log(sum)
 }
 even()
+                                                                                                  
+var arr=[10,20,30,40,50,60]
+for(var index in arr)
+{
+  console.log(index)
+}
+for(var index of arr)
+  {
+    console.log(index)
+  } 
+
+var obj={ name:"sarguna",dept:"Cs"}                                                                                             
+for( var key in obj)
+{
+  console.log(key)
+  console.log(obj[key])
+}  
+
+var arr=[1,2,3,4,5]
+var arr2=[...arr,6,7,8]
+console.log(arr2)
+
+var marks=[90,99,97,98,100]
+var[m1,m2,m3,m4,m5]=marks;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+
+function saybye()
+{
+  console.log("bye")
+}
+function add(name,callback)
+{
+ console.log( "hello  " + name)
+ callback()
+}
+add("sarguna",saybye);
+
+const btn=document.getElementById("btn1");
+btn.onclick=function()
+{
+  window.open("https://www.instagram.com")
+  alert("welcome")
+}
+
--- script.js
+++ script.js
@@ -10,10 +10,12 @@ function add(a,b)
   console.log(a-b)
 }
 add(10,5)
+
 const sum=()=>{
   console.log(10+20)
 }
 sum()
+
 a=20
 if(a%2==0)
 {
@@ -22,17 +24,20 @@ if(a%2==0)
 else{
   console.log("odd")
 }
+
 i=1
 while(i<=10)
 {
   console.log(i)
   i++
 }
+
 i=1
 do{
   console.log(i)
 }
 while(i<=10);
+
 for(i=1;i<=10;i++)
 {
   if(i%2==0)
@@ -40,6 +45,7 @@ for(i=1;i<=10;i++)
     console.log(i)
   }
 }
+
 n=3
 switch(n){
    case 1:
@@ -52,6 +58,7 @@ switch(n){
      console.log("friday")
      break;    
 }
+
 n=10
 var result=(n%2==0)?"even":"odd";
 console.log(result)
